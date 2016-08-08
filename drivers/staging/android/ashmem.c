0796412b235bfc68bc2f513616a827f99106a63b
Sunil Khatri <sunilkh@codeaurora.org>
2016-08-08 15:06:12 +0200

    ashmem: Validate ashmem memory with fops pointer
    Validate the ashmem memory entry against f_op pointer
    rather then comparing its name with path of the dentry.
    
    This is to avoid any invalid access to ashmem area in cases
    where some one deliberately set the dentry name to /ashmem.
    
    Change-Id: I74e50cd244f68cb13009cf2355e528485f4de34b

--- drivers/staging/android/ashmem.c
+++ drivers/staging/android/ashmem.c
@@ -766,11 +766,22 @@ static long compat_ashmem_ioctl(struct file *file, unsigned int cmd, unsigned lo
 }
 #endif
 
+static const struct file_operations ashmem_fops = {
+        ashmem_fops.owner = THIS_MODULE,
+        THIS_MODULE.open = ashmem_open,
+        ashmem_open.release = ashmem_release,
+        ashmem_release.read = ashmem_read,
+        ashmem_read.llseek = ashmem_llseek,
+        ashmem_llseek.mmap = ashmem_mmap,
+        ashmem_mmap.unlocked_ioctl = ashmem_ioctl,
+#ifdef CONFIG_COMPAT
+        CONFIG_COMPAT.compat_ioctl = compat_ashmem_ioctl,
+#endif
+};
+
 static int is_ashmem_file(struct file *file)
 {
-	char fname[256], *name;
-	name = dentry_path(file->f_dentry, fname, 256);
-	return strcmp(name, "/ashmem") ? 0 : 1;
+	return (file->f_op == &ashmem_fops);
 }
 
 int get_ashmem_file(int fd, struct file **filp, struct file **vm_file,
@@ -818,19 +829,6 @@ void put_ashmem_file(struct file *file)
 		fput(file);
 }
 EXPORT_SYMBOL(put_ashmem_file);
-
-static const struct file_operations ashmem_fops = {
-	.owner = THIS_MODULE,
-	.open = ashmem_open,
-	.release = ashmem_release,
-	.read = ashmem_read,
-	.llseek = ashmem_llseek,
-	.mmap = ashmem_mmap,
-	.unlocked_ioctl = ashmem_ioctl,
-#ifdef CONFIG_COMPAT
-	.compat_ioctl = compat_ashmem_ioctl,
-#endif
-};
 
 static struct miscdevice ashmem_misc = {
 	.minor = MISC_DYNAMIC_MINOR,
